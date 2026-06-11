82b3ffa106ba74b82c27de6fb76450f0b105c2eb
devarthuraugusto <arthuraugusto2102@icloud.com>
2026-06-10 21:52:23 -0300

    adicionei umas parada

--- src/funcoes.py
+++ src/funcoes.py
@@ -9,19 +9,25 @@ def cronometro():
 
 
 def pontuar(pergunta, pontuacao_atual, resposta):
-    # Esta função agora calcula e RETORNA a nova pontuação
+    '''
+    Esta função calcula e RETORNA a pontuação atualizada do jogador.
+    '''
+
     if resposta == pergunta.alternativa_correta:
         if pergunta.dificuldade == 'facil':
-            pontuacao_atual += 10
+            pontuacao_atual += 1
         elif pergunta.dificuldade == 'media':
-            pontuacao_atual += 20
+            pontuacao_atual += 2
         elif pergunta.dificuldade == 'dificil':
-            pontuacao_atual += 30
+            pontuacao_atual += 3
     return pontuacao_atual # Retorna o novo valor para quem chamou
 
 def perder_vida(vidas_atuais):
-    # Esta função calcula e RETORNA a quantidade de vidas
+    '''
+    Esta função calcula e RETORNA a quantidade de vidas atualizada do jogador.
+    '''
     vidas_atuais -= 1
     return vidas_atuais
+
 
 
